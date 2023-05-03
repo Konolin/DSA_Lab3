cb3b4cb32c9f722281072fe18c7820228680aa4c
Mandru Catalin <103405174+Konolin@users.noreply.github.com>
2023-05-03 10:02:25 +0300

    Update ListIterator.cpp

--- DSA_Lab3/ListIterator.cpp
+++ DSA_Lab3/ListIterator.cpp
@@ -2,24 +2,34 @@
 #include "IndexedList.h"
 #include <exception>
 
+
+// worst = average = best = θ(n)
 ListIterator::ListIterator(const IndexedList &list) : list(list) {
     index = list.head;
 }
 
+
+// worst = average = best = θ(1)
 void ListIterator::first() {
     index = list.head;
 }
 
+
+// worst = average = best = θ(1)
 void ListIterator::next() {
     if (valid()) index = list.next[index];
     else throw std::exception();
 }
 
+
+// worst = average = best = θ(1)
 bool ListIterator::valid() const {
     return index != -1;
 }
 
+
+// worst = average = best = θ(1)
 TElem ListIterator::getCurrent() const {
-    if (!valid()) throw std::exception();
-    return list.elements[index];
+    if (valid()) return list.elements[index];
+    throw std::exception();
 }
